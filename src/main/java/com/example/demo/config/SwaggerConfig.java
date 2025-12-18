47207fbd4dcc95f9a180a27bea4d3b883aaf17a6
bala-amypo <bala@amypo.in>
2025-12-18 08:57:39 +0000

    User commit

---
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -0,0 +1,11 @@
+package com.example.demo.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+        info = @Info(title = "Hostel Roommate Matcher API",
+                     version = "1.0")
+)
+public class SwaggerConfig {
+}
