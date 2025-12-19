a6e223cb1f3be9d6aaf5c7de3788b6c8b93c3aa8
bala-amypo <bala@amypo.in>
2025-12-19 09:06:15 +0000

    User commit

---
+++ src/main/java/com/example/demo/Repository/MatchResultRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.MatchResult;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface MatchResultRepository extends JpaRepository<MatchResult, Long> {
+
+    List<MatchResult> findByStudentAIdOrStudentBIdOrderByScoreDesc(Long a, Long b);
+}
