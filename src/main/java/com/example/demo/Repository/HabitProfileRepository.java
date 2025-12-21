bc71346302597cea8527bd95eafd1bb2410bfa90
bala-amypo <bala@amypo.in>
2025-12-21 12:58:15 +0000

    User commit

--- src/main/java/com/example/demo/Repository/HabitProfileRepository.java
+++ src/main/java/com/example/demo/Repository/HabitProfileRepository.java
@@ -1,3 +1,15 @@
-public interface HabitProfileRepository extends JpaRepository<HabitProfile, Long> {
+package com.example.demo.repository;
+
+import com.example.demo.model.HabitProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.List;
+
+public interface HabitProfileRepository
+        extends JpaRepository<HabitProfile, Long> {
+
     Optional<HabitProfile> findByStudentId(Long studentId);
+
+    List<HabitProfile> findAll();
 }
--- src/main/java/com/example/demo/Repository/StudentProfileRepository.java
+++ src/main/java/com/example/demo/Repository/StudentProfileRepository.java
@@ -1,11 +1,14 @@
 package com.example.demo.repository;
 
+import com.example.demo.model.StudentProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.model.*;
 
 import java.util.Optional;
-import java.util.List;
 
-public interface StudentProfileRepository extends JpaRepository<StudentProfile, Long> {
+public interface StudentProfileRepository
+        extends JpaRepository<StudentProfile, Long> {
+
     Optional<StudentProfile> findByStudentId(String studentId);
+
+    Optional<StudentProfile> findByEmail(String email);
 }
