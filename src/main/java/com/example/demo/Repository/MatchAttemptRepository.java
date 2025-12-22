c3d807a8dc3e66f7cbaaec758b0cf06bcef6137a
bala-amypo <bala@amypo.in>
2025-12-22 08:03:44 +0000

    User commit

--- src/main/java/com/example/demo/Repository/MatchAttemptRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.MatchAttempt;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MatchAttemptRepository
-        extends JpaRepository<MatchAttempt, Long> {
-}
--- src/main/java/com/example/demo/Repository/RoomAssignmentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.RoomAssignment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoomAssignmentRepository
-        extends JpaRepository<RoomAssignment, Long> {
-}
