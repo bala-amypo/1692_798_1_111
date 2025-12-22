88a3636f657582a24e630cc1baedd8ab726bd14b
bala-amypo <bala@amypo.in>
2025-12-22 07:42:44 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,21 +1,12 @@
-// package com.example.demo;
+package com.example.demo;
 
-// import org.springframework.boot.SpringApplication;
-// import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// @SpringBootApplication
-// public class DemoApplication {
+@SpringBootApplication
+public class DemoApplication {
 
-// 	public static void main(String[] args) {
-// 		SpringApplication.run(DemoApplication.class, args);
-// 	}
-
-// }
-spring.datasource.url=jdbc:mysql://localhost:3306/hostel_db
-spring.datasource.username=root
-spring.datasource.password=your_password
-
-spring.jpa.hibernate.ddl-auto=update
-spring.jpa.show-sql=true
-spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.MySQLDialect
-
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
